fb3a4f4d4d65af843fb4db46b849e89e0b8e2400
Oleksander Lomskyi <ylomsky@gmail.com>
2026-02-03 17:51:54 +0200

    Initial frontend setup.

---
+++ frontend/src/App.tsx
@@ -0,0 +1,10 @@
+import "./App.css"
+import { Button } from "@heroui/react";
+
+export const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <Button color="primary" >Button</Button>
+    </header>
+  </div>
+)
